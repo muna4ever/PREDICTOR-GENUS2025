eb3a4528b0cfaf7cc259828af97fb301b89e6559
muna4ever <munachichukwunenye@gmail.com>
2025-05-23 16:18:01 +0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,14 @@
-import React, { useState } from "react";
+import React from "react";
 import FootballMatches from "./components/FootballMatches";
 
 function App() {
-  // Only one source site, so hardcode it and hide the dropdown if you want.
-  const [site, setSite] = useState("football-data");
-
   return (
     <div className="App">
       <h1>Football Data Viewer</h1>
-      {/* Select Source Site dropdown shows only football-data.org */}
-      <label>
-        Select Source Site:{" "}
-        <select value={site} onChange={e => setSite(e.target.value)}>
-          <option value="football-data">football-data.org</option>
-        </select>
-      </label>
-      <FootballMatches site={site} />
+      <div>
+        Source Site: <b>football-data.org</b>
+      </div>
+      <FootballMatches site="football-data" />
     </div>
   );
 }
